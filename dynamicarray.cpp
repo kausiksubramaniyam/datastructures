e331de0813b04904d4f42b0533ce61ea2aefc706
kausiksubramaniyam <59435311+kausiksubramaniyam@users.noreply.github.com>
2021-09-29 19:46:44 +0530

    Update dynamicarray.cpp

--- dynamicarray.cpp
+++ dynamicarray.cpp
@@ -1,25 +1,36 @@
 #include<iostream>
 #include<vector>
 using namespace std;
+vector<int> dynarray(vector<vector<int>> queries,int n){
+    vector<vector<int>> arr(n);
+    vector<int> ans;
+    int idx=0,lastans=0;
+    for(int i=0;i<queries.size();i++){
+        if(queries[i][0]==1){
+            idx=((queries[i][1]^lastans)%n);
+            arr[idx].push_back(queries[i][2]);
+        }
+        else if(queries[i][0]==2){
+            idx=((queries[i][1]^lastans)%n);
+            lastans=arr[idx][queries[i][2]%(arr[idx].size())];
+            ans.push_back(lastans);
+            cout<<lastans<<"\n";
+        }
+    }
+    return ans;
+    
+}
 int main()
 {
-    int n,i,lastans,idx,y;
+    int n,q,no;
     cin>>n;
-    vector<vector<int>> a(n);
-    int q[3];
-    for(i=0;i<3;i++){
-        cin>>q[i];
+    cin>>q;
+    vector<vector<int>> queries(q);
+    for(int i=0;i<q;i++){
+        for(int j=0;j<3;j++){
+            cin>>no;
+            queries[i].push_back(no);
+        }
     }
-    if(q[0]==1){
-        idx=(q[1]^lastans)%n;
-        a[idx].push_back(q[2]);
-    }
-    else if(q[0]==2){
-        cout<<"HI";
-        idx=(q[1]^lastans)%n;
-        y=q[2]%(a[idx].size());
-        lastans=a[idx][y];
-        cout<<"HI"<<lastans;
-    }
-    
+    dynarray(queries,n);
 }
