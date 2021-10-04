6d3acd0bf7a343b3b946b2236e6f9cc8356efdff
kausiksubramaniyam <59435311+kausiksubramaniyam@users.noreply.github.com>
2021-10-04 15:34:07 +0530

    Update RomanToInteger.cpp

--- RomanToInteger.cpp
+++ RomanToInteger.cpp
@@ -1,64 +1,25 @@
 class Solution {
 public:
     int romanToInt(string s) {
-    int i,v=0;
-    for(i=0;i<s.size();i++){
-        if((s[i]=='V')&&(s[i-1]=='I')){
-            s[i]='A';
-            s[i-1]='A';
-            v=4;
+    int val,i,no=0,fin=0;
+    for(i=s.size()-1;~i;i--){
+        switch(s[i]){
+            case 'I' : val=1;break;
+            case 'V' : val=5;break;
+            case 'X' : val=10;break;
+            case 'L' : val=50;break;
+            case 'C' : val=100;break;
+            case 'D' : val=500;break;
+            case 'M' : val=1000;break;
         }
-        else if((s[i]=='X')&&(s[i-1]=='I')){
-            s[i]='A';
-            s[i-1]='A';
-            v=v+9;
+        if(no<=val){
+            no=val;
+            fin=fin+no;
         }
-        else if((s[i]=='L')&&(s[i-1]=='X')){
-            s[i]='A';
-            s[i-1]='A';
-            v=v+40;
-        }
-        else if((s[i]=='C')&&(s[i-1]=='X')){
-            s[i]='A';
-            s[i-1]='A';
-            v=v+90;
-        }
-        else if((s[i]=='D')&&(s[i-1]=='C')){
-            s[i]='A';
-            s[i-1]='A';
-            v=v+400;
-        }
-        else if((s[i]=='M')&&(s[i-1]=='C')){
-            s[i]='A';
-            s[i-1]='A';
-            v=v+900;
+        if(no>val){
+            fin=fin-val;
         }
     }
-    for(i=0;i<s.size();i++){
-        if(s[i]!='A'){
-            if(s[i]=='I'){
-                v=v+1;
-            }
-            else if(s[i]=='V'){
-                v=v+5;
-            }
-            else if(s[i]=='X'){
-                v=v+10;
-            }
-            else if(s[i]=='L'){
-                v=v+50;
-            }
-            else if(s[i]=='C'){
-                v=v+100;
-            }
-            else if(s[i]=='D'){
-                v=v+500;
-            }
-            else if(s[i]=='M'){
-                v=v+1000;
-            }
-        }
-    }
-    return v;
+    return fin;  
     }
 };
