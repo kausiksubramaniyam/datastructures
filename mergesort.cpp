922e7210afc95f5451857e73de070a181f080bc3
kausiksubramaniyam <59435311+kausiksubramaniyam@users.noreply.github.com>
2021-10-23 19:22:22 +0530

    Incomplete ,but updates in merge function

--- mergesort.cpp
+++ mergesort.cpp
@@ -1,55 +1,53 @@
 #include<iostream>
 using namespace std;
-int split(int a[],int length){
-    int mid=length/2;
-    int b[mid],c[length-mid];
-    cout<<mid<<" "<<length-mid;
-    int i=0,j=0;
-    for(i=0;i<mid;i++){
-        b[j]=a[i];
-        j++;
-    }
-    j=0;
-    for(i=mid;i<length;i++){
-        c[j]=a[i];
-        j++;
+void merge(int arr[10],int lb,int mid,int ub){
+    int i=lb;
+    int j=mid+1;
+    int k=0;
+    int b[10],c[10];
+    for(i=lb;i<=mid;i++){
+        b[i]=arr[i];
     }
     i=0;
-    j=0;
-    int k=0,fin[10];
-    while(i<mid && j<length-mid){
-        if(b[i]<c[j]){
-            fin[k]=b[i];
+    for(j=mid+1;j<ub;j++){
+        c[i]=arr[j];
+        i++;
+    }
+    while(i<=mid && j<ub){
+        if(arr[i]<arr[j]){
+            b[k]=arr[i];
             i++;
         }
         else{
-            fin[k]=c[j];
+            b[k]=arr[j];
             j++;
         }
         k++;
     }
-    while(i<mid){
-        fin[k]=b[i];
+    while(i<=mid){
+        b[k]=arr[i];
         i++;
         k++;
     }
-    while(j<length-mid){
-        fin[k]=c[j];
+    while(j<ub){
+        b[k]=arr[j];
         j++;
         k++;
     }
-    for(int i=0;i<k;i++){
-        cout<<fin[i];
+}
+void mergesort(int arr[10],int lb,int ub){
+    int mid;
+    if(lb<ub){
+        mid=(lb+ub)/2;
+        mergesort(arr,lb,mid);
+        mergesort(arr,mid+1,ub);
+        merge(arr,lb,mid,ub);
     }
-    return 0;
 }
 int main(){
-    int i,a[10];
-    int n;
-    cin>>n;
-    for(i=0;i<n;i++){
-        cin>>a[i];
-    }
-    split(a,n);
+    int n,lb,ub,arr[10]={12,11,13,5,6,7};
+    lb=0;
+    ub=5;
+    mergesort(arr,lb,ub);
     return 0;
 }
